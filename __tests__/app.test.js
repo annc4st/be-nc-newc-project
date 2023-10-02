09220bbd93ee54f88ce675c0fb32855dbdbda2ac
Ann <annaseniuta@gmail.com>
2023-10-02 12:23:08 +0100

    2task get/api/topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,46 @@
+const app = require('../db/app');
+const db = require('../db/connection');
+
+const request = require('supertest');
+const seed = require('../db/seeds/seed.js');
+const {articleData, commentData, topicData, userData} = require('../db/data/test-data/index');
+const {getTopics} = require('../controllers/controllers.js');
+
+
+
+beforeEach(() =>{
+    return seed({ articleData, commentData, topicData, userData })
+    })
+afterAll(() => {
+    db.end()
+})
+
+describe('General testing', () => {
+    test('should return 404 if path spelt wrong or does not exist', () =>{
+        return request(app)
+        .get('/api/not-existing-path')
+        .expect(404)
+        .then((response) => {
+            expect(response.body.message).toBe('path is not found')
+        })
+    });
+})
+
+describe.only('GET /api/topics', () =>{
+    test('responds with status 200', () =>{
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+    });
+
+    test('returns array of topics', () =>{
+        return request(app)
+        .get('/api/topics')
+        .then(({body}) => {
+            body.topics.forEach((topic) => {
+                expect(typeof(topic.description)).toBe('string');
+                expect(typeof(topic.slug)).toBe('string')
+            })
+        })
+    });
+})
